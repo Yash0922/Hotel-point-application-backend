24dbb577591cfdda45d8cf3044ddaee5f451993a
yash0922 <yashkumarpal987@gmail.com>
2023-08-10 20:46:12 +0530

    ready-for deploy

--- models/Users.js
+++ models/Users.js
@@ -5,6 +5,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    Email: {
+        type: String,
+        required: true
+    },
     password : {
         type: String,
         required: true
